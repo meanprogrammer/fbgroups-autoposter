e60cb8f8d3bca34c8aa89037ef6317f6353df683
Valiant <valiantdudz@yahoo.com>
2017-02-26 16:13:14 +0800

    test

--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -13,27 +13,27 @@ namespace ConsoleTester
         static void Main(string[] args)
         {
             FacebookLoginPage login = new FacebookLoginPage();
-            
+
             login.WaitForComplete();
 
 
-                //xhpc_message
+            //xhpc_message
 
             login.LogOn("dudz.business@gmail.com", "2Winter*8");
-                //hasText = somePage.ContainsText("Dixin");
+            //hasText = somePage.ContainsText("Dixin");
 
             login.ClearCache();
             login.ClearCookies();
-                
 
-                //https://www.facebook.com/groups/1698197500455137/yourposts/
 
-                 
-                //TO COMMENT:
-                //get permalink?????
-                //comment id
-                //add_comment_text
-            } 
-        }  
+            //https://www.facebook.com/groups/1698197500455137/yourposts/
+
+
+            //TO COMMENT:
+            //get permalink?????
+            //comment id
+            //add_comment_text
+        }
     }
 }
+
